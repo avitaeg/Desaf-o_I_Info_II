7236ba97da05f8b31a543d6d2f114b23d4189f2e
LinaM <linam.vanegasg@udea.edu.co>
2026-03-15 14:48:17 -0500

    //Validar tamaño del tablero en multiplos de 8//

--- Desafio_I/main.cpp
+++ Desafio_I/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 
-using namespace std;
+    using namespace std;
 
-int main()
-{
-    cout << "Hola desafío I" << endl;
+    int main()
+    {
+        int filas;
+        int columnas;
 
-    return 0;
-}
+        for (; ; )
+        {
+            cout<< "Ingrese un numero de filas que sea multiplo de 8: " ;
+            cin >> filas;
+
+            if (filas > 0 && filas % 8==0)
+            {
+                break;
+            }
+            cout<< "Intente nuevamente." << endl;
+            cout<< "Ingrese un numero que sea multiplo de 8 (8,16,24..). " << endl;
+        }
+
+        for (; ; )
+        {
+            cout<< "Ingrese un numero de columnas que sea multiplo de 8: " ;
+            cin >> columnas;
+
+            if (columnas > 0 && columnas % 8==0)
+            {
+                break;
+            }
+            cout<< "Intente nuevamente" << endl;
+            cout<< "Ingrese un numero que sea multiplo de 8 (8,16,24..)" << endl;
+        }
+
+        cout<< "Tablero creado correctamente";
+
+        return 0;
+    }
+
 
