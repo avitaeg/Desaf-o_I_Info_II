7aafc00393218d488eb0af24d19a7bfb6abb0dc0
LinaM <linam.vanegasg@udea.edu.co>
2026-03-17 22:39:53 -0500

    Generar_Figuras_Aleatorias

--- Desafio_I/funciones_lina.cpp
+++ Desafio_I/funciones_lina.cpp
@@ -53,9 +53,16 @@ void figura_I ( unsigned char* fila, int& Alto, int& Ancho)
     fila[3] = 0b00000001;
 }
 
+void figura_O(unsigned char* fila, int& Alto, int& Ancho)
+{
+    Alto = 2;
+    Ancho = 2;
+    fila[0] = 0b00000011;
+    fila[1] = 0b00000011;
+}
 // Figura T
 
-void figura_O (unsigned char* fila, int& Alto, int& Ancho)
+void figura_T (unsigned char* fila, int& Alto, int& Ancho)
 {
     Alto = 2;
     Ancho = 3;
@@ -123,5 +130,60 @@ void Rotarfigura(unsigned char* figuraOriginal, unsigned char* figuraRotada, int
     }
 }
 
+//GENERAR FIGURAS DE MANERA ALEATORIA
 
+#include <cstdlib>
+#include <ctime>
 
+void generarFiguraAleatoria(unsigned char* fila, int &alto, int &ancho)
+{
+    int opcion = rand() % 7;
+
+    switch(opcion)
+    {
+    case 0:
+        figura_I(fila, alto, ancho);
+        break;
+    case 1:
+        figura_O(fila, alto, ancho);
+        break;
+    case 2:
+        figura_Z(fila, alto, ancho);
+        break;
+    case 3:
+        figura_L(fila, alto, ancho);
+        break;
+    case 4:
+        figura_J(fila, alto, ancho);
+        break;
+    case 5:
+        figura_S(fila, alto, ancho);
+        break;
+    case 6:
+        figura_Z(fila, alto, ancho);
+        break;
+    }
+}
+// GENERAR UN NUMERO ENTRE 1 Y 7
+int main()
+{
+    srand(time(0));
+
+    unsigned char figura[4]; // tamaño máximo
+    int alto, ancho;
+
+    generarFiguraAleatoria(figura, alto, ancho);
+
+    cout << "Figura generada: " << endl;
+
+    for(int i = 0; i < alto; i++)
+    {
+        for(int j = 0; j < ancho; j++)
+        {
+            if(figura[i] & (1 << j))
+                cout << "[] ";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
